dd6a4c5f7c00eb5511b538a0d5dff37eccba3f17
Daojing <daojing.zhai@gmail.com>
2018-08-22 18:16:45 -0500

    Renew

--- 5. Data Mining Project/2.Stock Price Distribution/log_return.m
+++
@@ -1,4 +0,0 @@
-function [logreturn] = log_return(simplereturn)
-    % find log-return of the stock market given prices of stock everyday. 
-    logreturn = log(1+simplereturn);
-end
--- 5. Data Mining Project/2.Stock Price Distribution/simple_return.m
+++
@@ -1,7 +0,0 @@
-function [simplereturn] = simple_return(pricedata)
-    % find log-return of the stock market given prices of stock everyday. 
-    simplereturn = ones(length(pricedata)-1,1);
-    for i=1:length(simplereturn)
-        simplereturn(i) = (pricedata(i+1)-pricedata(i))/pricedata(i);
-    end
-end
